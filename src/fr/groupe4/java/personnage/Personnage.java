3686139c27088686b7e267654305e49681e78a94
Unknown <thibaud.roussel@edu.itescia.fr>
2019-01-14 16:50:13 +0100

    = 8

--- src/fr/groupe4/java/personnage/Personnage.java
+++ src/fr/groupe4/java/personnage/Personnage.java
@@ -2,10 +2,10 @@ package fr.groupe4.java.personnage;
 
 public class Personnage {
 
-    int vie;
-    int force;
-    int dexterité;
-    int intelligence;
+    int vie = 8;
+    int force = 8;
+    int dexterité = 8;
+    int intelligence; = 8
     public Personnage() {
 
     }
